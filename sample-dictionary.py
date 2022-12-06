366b71aa285f6975ab506d38e4d6cc1c14da51ca
Ernest Gibbs III <beg0991@yahoo.com>
2022-12-06 14:42:58 +0000

    adding sample dictionary

---
+++ sample-dictionary.py
@@ -0,0 +1,19 @@
+var_dict = {}
+
+print(type(var_dict))
+
+objs = {"fruit" : "grapes", "number" : "7"}
+
+print(objs)
+
+objs["food"] = "pizza"
+objs[1] = 493
+
+print(objs)
+
+objs["food"] = "fish filet"
+objs["Food"] = "fish filet" # these are different outputs
+print(objs)
+
+for k, v in objs.items():
+    print(k,v)
