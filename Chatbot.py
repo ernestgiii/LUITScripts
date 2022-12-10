f86c3b8a39289d036fb85b9a632d6f0339d3827d
Ernest Gibbs III <beg0991@yahoo.com>
2022-12-10 20:51:35 +0000

    updated chatbot

--- Chatbot.py
+++ Chatbot.py
@@ -15,7 +15,7 @@ like = input("Would you like to play a game?")
 
 time.sleep(2)
 if "yes" in like:
-    print("Ok! What is your favorite comic company Marvel or Breezy Comics?")
+    print("Ok! What is your favorite comic company Marvel or Breezy Comics 🦸🏾?")
 else:
     print("Sorry! We'll play another time")
     exit()
@@ -24,10 +24,10 @@ results = input("Please Enter your favorite comic company?: ")
 if "Marvel" in results:
     print("I hear Namor is one of your favorite characters, do you like the X-Men too?")
 if "Breezy Comics" in results:
-    print("Black Thunder is the greatest!")
+    print("Black Thunder is the greatest! ⚡")
 time.sleep(2)
 
-print("I hope youe enjoyed your experience with the EGen chatbot. I hope you have a good day!")
+print("I hope you enjoyed your experience with the EGen chatbot. I hope you have a good day!")
 
     
     
