c98d59a54159a98d9cc436ead0bf29e2f3754d1f
Ernest Gibbs III <beg0991@yahoo.com>
2022-12-10 14:58:26 +0000

    adding a convo bot to repo

---
+++ Chatbot.py
@@ -0,0 +1,34 @@
+# We're creating a basic chatbot 
+
+import time 
+
+
+name = input ("Hello! I am EGen, Ernest's new chatbot. What is your name: ")
+
+time.sleep(2)
+
+print("Hello" ' '+  name)
+
+time.sleep(2)
+
+like = input("Would you like to play a game?")
+
+time.sleep(2)
+if "yes" in like:
+    print("Ok! What is your favorite comic company Marvel or Breezy Comics?")
+else:
+    print("Sorry! We'll play another time")
+    exit()
+time.sleep(2)
+results = input("Please Enter your favorite comic company?: ")
+if "Marvel" in results:
+    print("I hear Namor is one of your favorite characters, do you like the X-Men too?")
+if "Breezy Comics" in results:
+    print("Black Thunder is the greatest!")
+time.sleep(2)
+
+print("I hope youe enjoyed your experience with the EGen chatbot. I hope you have a good day!")
+
+    
+    
+    
