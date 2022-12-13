e115a214b82a537a79053e5908fda6bc2a8bb201
Ernest Gibbs III <beg0991@yahoo.com>
2022-12-13 13:23:43 +0000

    adding s3 creation date script

---
+++ creation-dates3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.7
+
+# We're creating a creation date for s3 bucket
+
+import boto3
+
+s3_resource=boto3.client
+s3_resource=boto3.client("s3")
+s3_resource.list_buckets()["Buckets"][0]["Name"]
+creation_date=s3_resource.list_buckets()["Buckets"][0]["CreationDate"]
+creation_date.strftime("%d%m%y_%H:%M:%s")
+
+for bucket in s3_resource.list_buckets()["Buckets"]:
+    print(bucket["Name"])
+    print(bucket["CreationDate"]) 
