4e4336004db067f13cac3c3d48d7863546801789
Ernest Gibbs III <beg0991@yahoo.com>
2022-12-15 17:04:37 +0000

    adding updates to the script

--- Week14Project.py
+++ Week14Project.py
@@ -4,16 +4,22 @@ import boto3
 
 ec2 = boto3.resource('ec2')
 
-#instance = ec2.create_instances(
-    #ImageId='ami-0b0dcb5067f052a63',
-    #MinCount=3,
-    #MaxCount=3,
-    #InstanceType='t2.micro',
-   # )
-#print(instance)
+instance = ec2.create_instances(
+    ImageId='ami-0b0dcb5067f052a63',
+    MinCount=3,
+    MaxCount=3,
+    InstanceType='t2.micro',
+    )
+print(instance)
 
 # We'll be stopping our instances now 
 
 ec2.Instance('i-06ee3df3958780598').stop()
 ec2.Instance('i-06663687c94affc1a').stop()
 ec2.Instance('i-094209236835c2c7b').stop()
+
+# We're going to stop our instances that have tags on them 
+
+ec2_filter=[{'Name': 'tag:Dev Team', 'Values': ['dev']}, {'Name': 'instance-state-name', 'Values': ['running']}]
+
+ec2.instances.filter(Filters=ec2_filter).stop()
