8eb2561181b0472d953f6c2827fb95b441067dcd
Ernest Gibbs III <beg0991@yahoo.com>
2022-12-16 11:57:45 +0000

    Adding Dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,7 +1,7 @@
 # Set the emails variable to be an empty dictionary
 emails = {}
 
-assert emails == {}, f"Expected `emails` to be {{}} but got: {repr(users)}"
+assert emails == {}, f"Expected `emails` to be {{}} but got: {repr(emails)}"
 
 # Add Ashley, Craig, and Elizabeth to the emails dictionary without reassigning the variable
 
